c221da863227990ba5cb7623d9f0c929db35d879
abudaan <daniel@abumarkub.net>
2017-09-30 10:55:51 +0200

    removed canvas-prebuilt

--- src/util/canvas/canvas.js
+++ src/util/canvas/canvas.js
@@ -1,4 +1,4 @@
-import {domCreate} from '../dom';
+import { domCreate } from '../dom';
 
 export var Canvas;
 
@@ -6,16 +6,17 @@ try {
   // try to load canvas module
   Canvas = require('canvas');
 } catch (e) {
-  try {
-    // if canvas fails, try to load canvas-prebuilt
-    Canvas = require('canvas-prebuilt');
-  } catch (e2) {
-    // if all options fail, set to null
-    Canvas = null;
-  }
+  // try {
+  //   // if canvas fails, try to load canvas-prebuilt
+  //   Canvas = require('canvas-prebuilt');
+  // } catch (e2) {
+  //   // if all options fail, set to null
+  //   Canvas = null;
+  // }
+  Canvas = null;
 }
 
-export default function(w, h) {
+export default function (w, h) {
   var canvas = domCreate(null, 'canvas');
   if (canvas && canvas.getContext) {
     canvas.width = w;
